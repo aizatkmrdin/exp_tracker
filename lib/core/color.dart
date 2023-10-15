c68b657dfce9afd44f27bdf69df3791099143893
aizatkmrdin <59000200+aizatkmrdin@users.noreply.github.com>
2023-10-16 07:09:44 +0800

    first commit

---
+++ lib/core/color.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DarkMode {
+  Color bgColor = const Color(0xFF060907);
+  Color bgCardColor = const Color(0xFF191A1F);
+  Color secondaryColor = const Color(0xFF767676);
+  Color bgButtonColor = const Color(0xFF2D2D2D);
+  Color titleTextColor = Colors.white;
+}
+
+class LightMode {
+  Color bgColor = Colors.white;
+  Color bgCardColord = const Color(0xFFFBFBFB);
+  Color secondaryColor = const Color(0xFFF6F6F6);
+  Color bgButtonColor = const Color(0xFF3C3C3C);
+  Color titleTextColor = Colors.black;
+}
